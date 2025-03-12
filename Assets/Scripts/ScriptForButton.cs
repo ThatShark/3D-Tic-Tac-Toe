db7742759d23b34d3c3e0489e41adcfdd9ee9399
HyperBiotic <mickey.changchien@gmail.com>
2025-03-12 23:52:55 +0800

    camera

--- Assets/Scripts/ScriptForButton.cs
+++ Assets/Scripts/ScriptForButton.cs
@@ -1,18 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScriptForButton : MonoBehaviour{
-    public Vector3 mousePosition = Input.mousePosition;
 
     void Start() {
         
     }
-
     void Update() {
-       if(Input.GetMouseButtonDown(0)){
-            if(mousePosition.x > 0 && mousePosition.x < 100 && mousePosition.y > 0 && mousePosition.y < 100){
-                Debug.Log("Button Clicked");
-            }
-        }
-        
-    }
+       
+    }  
 }
