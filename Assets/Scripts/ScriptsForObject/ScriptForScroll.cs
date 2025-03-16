14ad068c2bc0659b6d90a11e04ee1f5195b96c11
ThatShark <51602017e@gmail.com>
2025-03-16 11:38:45 +0800

    Add Scroll Function

---
+++ Assets/Scripts/ScriptsForObject/ScriptForScroll.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptForScroll : MonoBehaviour {
+    public ScrollRect scrollRect;
+    public float scrollSensitivity = 1f;  // ºu°ÊÆF±Ó«×
+    void Update() {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollInput != 0f) {
+            scrollRect.verticalNormalizedPosition += scrollInput * scrollSensitivity;
+            scrollRect.verticalNormalizedPosition = Mathf.Clamp(scrollRect.verticalNormalizedPosition, 0f, 1f);
+        }
+    }
+}
