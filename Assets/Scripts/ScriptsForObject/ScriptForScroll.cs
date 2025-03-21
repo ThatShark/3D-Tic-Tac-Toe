5d6398460995dd8f71e6b1451107cf6af32d86d5
ThatShark <51602017e@gmail.com>
2025-03-21 10:10:41 +0800

    再次修改亂碼

--- Assets/Scripts/ScriptsForObject/ScriptForScroll.cs
+++ Assets/Scripts/ScriptsForObject/ScriptForScroll.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ScriptForScroll : MonoBehaviour {
     public ScrollRect scrollRect;
-    public float scrollSensitivity = 1f;  // ½Õ¾ãºu½üÆF±Ó«×
+    public float scrollSensitivity = 1f;  // èª¿æ•´æ»¾å‹•é€Ÿåº¦
     void Update() {
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
 
